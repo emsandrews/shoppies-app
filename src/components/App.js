293203bf44cb2b3b652495ed48ffc9492fbc5229
emsandrews <74979194+emsandrews@users.noreply.github.com>
2021-05-06 13:18:39 -0700

    converted App to class component

--- src/components/App.js
+++ src/components/App.js
@@ -7,85 +7,94 @@ import styled from "styled-components";
 
 const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=bd902656&"; // you should replace this with yours
 
-const App = () => {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const [errorMessage, setErrorMessage] = useState(null);
+const ErrorMessage = styled.div`
+  margin: auto;
+  font-weight: bold;
+  color: rgb(161, 15, 15);
+`;
 
-  useEffect(() => {
-    fetch(MOVIE_API_URL)
-      .then((response) => response.json())
-      .then((jsonResponse) => {
-        setMovies(jsonResponse.Search);
-        setLoading(false);
-      });
-  }, []);
+const AppWrapper = styled.div`
+  text-align: center;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+`;
 
-  const search = (searchValue) => {
-    setLoading(true);
-    setErrorMessage(null);
+const MoviesWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const NomWrapper = styled.div`
+  height: 300px;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.search = this.search.bind(this);
+    this.state = {
+      loading: true,
+      movies: [],
+      errorMessage: null,
+    };
+  }
+
+  componentDidMount() {
+    this.search("man");
+  }
+
+  search(searchValue) {
+    this.setState({
+      loading: true,
+      setErrorMessage: null,
+    });
 
     fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=bd902656&`)
       .then((response) => response.json())
       .then((jsonResponse) => {
         if (jsonResponse.Response === "True") {
-          setMovies(jsonResponse.Search);
-          setLoading(false);
+          this.setState({
+            movies: jsonResponse.Search,
+            loading: false,
+          });
         } else {
-          setErrorMessage(jsonResponse.Error);
-          setLoading(false);
+          this.setState({
+            errorMessage: jsonResponse.Error,
+            loading: false,
+          });
         }
       });
-  };
+  }
 
-  const ErrorMessage = styled.div`
-    margin: auto;
-    font-weight: bold;
-    color: rgb(161, 15, 15);
-  `;
-
-  const AppWrapper = styled.div`
-    text-align: center;
-    padding: 20px;
-    display: flex;
-    flex-direction: column;
-  `;
-
-  const MoviesWrapper = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    flex-direction: row;
-    justify-content: flex-start;
-    align-items: center;
-  `;
-
-  const NomWrapper = styled.div`
-    height: 300px;
-    width: 100%;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: flex-start;
-  `;
-
-  return (
-    <AppWrapper>
-      <Header text="The Shoppies 2021" />
-      <NomWrapper></NomWrapper>
-      <Search search={search} />
-      <MoviesWrapper>
-        {loading && !errorMessage ? (
-          <span>loading...</span>
-        ) : errorMessage ? (
-          <ErrorMessage>{errorMessage}</ErrorMessage>
-        ) : (
-          movies.map((movie, index) => (
-            <Movie key={`${index}-${movie.Title}`} movie={movie} />
-          ))
-        )}
-      </MoviesWrapper>
-    </AppWrapper>
-  );
-};
+  render() {
+    return (
+      <AppWrapper>
+        <Header text="The Shoppies 2021" />
+        <NomWrapper></NomWrapper>
+        <Search search={this.search} />
+        <MoviesWrapper>
+          {this.state.loading && !this.state.errorMessage ? (
+            <span>loading...</span>
+          ) : this.state.errorMessage ? (
+            <ErrorMessage>{this.state.errorMessage}</ErrorMessage>
+          ) : (
+            this.state.movies.map((movie, index) => (
+              <Movie key={`${index}-${movie.Title}`} movie={movie} />
+            ))
+          )}
+        </MoviesWrapper>
+      </AppWrapper>
+    );
+  }
+}
 
 export default App;
